53a6fce6e072bb0b312642ca252cdb570219c606
ZobninRoman <zobninroman@mail.ru>
2021-02-07 15:55:22 +0300

    Initial Commit

---
+++ SkillBox Lesson12/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SkillBox Lesson12
+//
+//  Created by Роман Зобнин on 07.02.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
